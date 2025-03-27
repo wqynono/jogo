24c7679823fee245a6860ffafb47e5d6f698e253
wangqiongyao <happy.work.258@gmail.com>
2025-03-27 17:13:11 +0800

    ad插件尝试

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,7 +3,7 @@ import GameGrid from "@/components/game-grid"
 import AdComponent from "@/components/ad" // 导入广告组件
 import GameSlide from "@/components/game-slide"
 import { categoryList, defaultGamelist } from "@/data/game"
-
+import { GoogleAdSense, ResponsiveAdUnit } from "nextjs-google-adsense";
 
 export default function HomePage() {
 
@@ -14,7 +14,19 @@ export default function HomePage() {
           <div className="mx-auto px-4 py-6">
             <GameGrid gameMobileLength={24} gamePcLength={45} />
             <div className="bg-white shadow-md p-4 my-4">
-              <AdComponent />
+              {/* <AdComponent /> */}
+              <GoogleAdSense publisherId="ca-pub-1279787934523230" />
+              {/* 3612947536 */}
+              <ResponsiveAdUnit
+                publisherId="ca-pub-1279787934523230"
+                slotId="4341944965"
+                type="after-home-hero" />
+
+
+              <ResponsiveAdUnit
+                publisherId="ca-pub-1279787934523230"
+                slotId="3612947536"
+                type="after-home-hero" />
             </div>
             <GameGrid gameMobileLength={18} gamePcLength={18} />
 
