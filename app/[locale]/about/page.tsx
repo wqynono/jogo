e3b6368c7ceb1ad4db22dc21cc9cc951d853fb69
wangqiongyao <happy.work.258@gmail.com>
2025-03-31 17:22:41 +0800

    about页面

---
+++ app/[locale]/about/page.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+    return (
+        <div>
+            <div className="min-h-screen">
+                <div className="min-h-screen max-w-12/12 m-0 mx-auto xl:max-w-11/12  ">
+                    <div className="mx-auto px-4 py-6">
+                        <div className="bg-white shadow-md p-4 my-4">
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
